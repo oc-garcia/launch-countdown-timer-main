dcd132c043b9cf7ef5450a2b37fd986075338db7
oc-garcia <octaviogarcia1988@gmail.com>
2022-12-30 13:50:57 -0300

    updating date

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ let hoursField = document.getElementById("hours");
 let minutesField = document.getElementById("minutes");
 let secondsField = document.getElementById("seconds");
 
-var countDownDate = new Date("Jun 15, 2026 16:00:00").getTime();
+var countDownDate = new Date("Nov 18, 2023 13:20:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
@@ -12,7 +12,7 @@ var x = setInterval(function () {
 
   // Find the distance between now and the count down date
   var distance = countDownDate - now;
-    
+
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
